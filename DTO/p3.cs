3023bbdd79867adcdf34236c97bd305a7a5affc7
DavCoder22 <davidelpatron22@gmail.com>
2024-02-03 18:49:46 -0500

    Version base

---
+++ DTO/p3.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArquitecturaG1.DTO
+{
+    public class p3 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

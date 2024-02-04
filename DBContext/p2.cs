bef922adaab60aef6d3c3cf616aebac5267527c6
Sebas <sebitas5225@gmail.com>
2024-02-04 15:25:53 -0500

    Revert "DAO-TAO V1.1"
    This reverts commit 849e43bdc2b4cbda29d2067c2345255d1f908e7a.

---
+++ DBContext/p2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArquitecturaG1.DBContext
+{
+    public class p2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
